e00a86b18cdde9338ee6d65a45ed8b984617d6cf
Hermann Mayer <jack@hermann-mayer.net>
2013-09-25 00:04:07 +0200

    [Backend] Added mongodb fixture and migration for user entity. Preparation for 0.3.0 release.

---
+++ database/migrations/mongodb.demo/20130924234048-add_users_indices.js
@@ -0,0 +1,38 @@
+/**
+ * Users Migration
+ */
+module.exports = {
+
+    // Forward-migration method
+    up: function(db, orm, done)
+    {
+        db.collection('users', function(err, collection) {
+
+            if (err) {
+                return done(err);
+            }
+
+            collection.ensureIndex({
+                'email': 1,
+                'fullname': 1
+            }, {
+                name: 'email_fullname',
+                unique: true,
+                background: true
+            }, done);
+        });
+    },
+
+    // Backward-migration method
+    down: function(db, orm, done)
+    {
+        db.collection('users', function(err, collection) {
+
+            if (err) {
+                return done(err);
+            }
+
+            collection.dropIndex('email_fullname', done);
+        });
+    }
+}
