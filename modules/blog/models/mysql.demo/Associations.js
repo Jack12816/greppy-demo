09948fdc08744e53cfc409bc846ec8548a4aef61
Hermann Mayer <jack@hermann-mayer.net>
2013-09-24 10:06:47 +0200

    [Backend] Updated mongodb and mysql models.

--- modules/blog/models/mysql.demo/Associations.js
+++ modules/blog/models/mysql.demo/Associations.js
@@ -6,22 +6,21 @@ module.exports = function(sequelize, models)
     /**
      * Posts
      */
-    
+
     // Post (1) --> (1) User
     models.Post.belongsTo(models.User, {as: 'Author', foreignKey: 'author_id'});
 
     // User (1) --> (n) Posts
     models.User.hasMany(models.Post, {as: 'PostsCreated', foreignKey: 'author_id'});
-    
+
     /**
      * Comments
      */
-    
+
     // Comment (1) --> (1) Post
     models.Comment.belongsTo(models.Post, {as: 'Comment', foreignKey: 'post_id'});
 
     // Post (1) --> (n) Comments
     models.Post.hasMany(models.Comment, {as: 'Comments', foreignKey: 'post_id'});
-    
-    
 };
+
